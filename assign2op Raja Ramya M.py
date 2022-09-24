9b3835cce8bae0e751cd846ec5c990bf17a6ba20
RajaRamyaM <99466533+RajaRamyaM@users.noreply.github.com>
2022-09-24 19:14:36 +0530

    Add files via upload

---
+++ assign2op Raja Ramya M.py
@@ -0,0 +1,8 @@
+Python 3.7.7rc1 (tags/v3.7.7rc1:93b7677f9c, Mar  4 2020, 12:05:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:/Users/USER/Python prog.py ====================
+temperature is 20
+humidity is 34
+alarm is not detected
+>>> 
